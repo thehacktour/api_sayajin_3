e6606d95265ca668b06777633965552e193b666d
Atilio <atiliogoulart@gmail.com>
2021-07-18 18:30:29 -0300

    1.3

--- app/settings.py
+++ app/settings.py
@@ -66,12 +66,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'sayajin',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                'host': 'localhost:27017'
+            }  
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

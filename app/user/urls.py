6c044b679439c04c99d513fe7e1ac503915c50bd
Atilio <atiliogoulart@gmail.com>
2021-07-18 18:29:59 -0300

    1.2

---
+++ app/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    path('all/', views.AllUsers.as_view()),
+    path('add/', views.AddUser.as_view()),
+
+]
--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import UserModel
+from .serializer import UserSerializer
+
+class AllUsers(APIView):
+
+    def get(self, request):
+
+        users = UserModel.objects.all()
+        user_serializer = UserSerializer(users, many=True)
+        return Response(user_serializer.data)
+    
+
+class AddUser(APIView):
+
+    def post(self, request):
+
+        data = {
+
+            'name':request.data.get('name'),
+            'age':request.data.get('age')
+
+        }
+
+        user_serializer = UserSerializer(data=data)
+
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        else:
+            return Response('Did not work!')

9e76dd2d4e5e4047a7612d9dc7ebbe4782f2bda3
Atilio <atiliogoulart@gmail.com>
2021-07-18 19:14:34 -0300

    1.9

--- app/user/urls.py
+++ app/user/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 
     path('all/', views.AllUsers.as_view()),
     path('add/', views.AddUser.as_view()),
+    path('<int:id>/', views.SpecificUser.as_view()),
 
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -31,3 +31,22 @@ class AddUser(APIView):
             return Response(user_serializer.data)
         else:
             return Response('Did not work!')
+
+class SpecificUser(APIView):
+
+    def get_user(self, id):
+
+        try:
+            return UserModel.objects.get(id=id)
+        except UserModel.DoesNotExist:
+            return Response('Does not exist')
+
+    def put(self, request, id):
+
+        user = self.get_user(id)
+        user_serializer = UserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        else:
+            return Response('Did not work')
